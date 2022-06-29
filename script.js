c4c15c0ddbf984f068cf18e6cbb18f1dc4aabc59
WishfulGrimm <mattsitunesacct@gmail.com>
2022-06-29 17:14:51 -0400

    Add basic hover functionality

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ for (i = 0; i < 256; i++){
     let div = document.createElement('div');
     div.classList.add('grid-item');
     div.id = i +1;
-    div.textContent = '.';
+    div.textContent = ' ';
     document.getElementById('container').appendChild(div);
 }
+
+let items = document.querySelectorAll('.grid-item');
+items.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        item.classList.toggle('hovering');
+    });
+});
