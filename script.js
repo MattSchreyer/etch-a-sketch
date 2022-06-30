56459fd4b14713ec5abe8ff9173a3fcdd29bef9f
WishfulGrimm <mattsitunesacct@gmail.com>
2022-06-29 20:34:40 -0400

    Begin work on random RGB assignment

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const html = document.querySelector('html');
-
 function run(){
     let col = prompt("Please enter the new width.");
     while (+col > 100){
@@ -12,28 +10,54 @@ function run(){
     build(+row, +col);
 }
 
+function hover(item){
+    //item.classList.toggle('hovering'); DEPRECATED
+    let style = item.style.backgroundColor;
+    let rgb = style.slice(4, -1);
+    let arr = rgb.split(", ");
+    console.log(arr.toString());
+    arr[0] = Math.floor(Math.random() * 361);
+    arr[1] = 0;
+    if (item.classList.contains('new')){
+        item.classList.remove('new');
+        arr[2] = 0;
+    }
+    else if (arr[2] == 100) arr[2] = arr[2];
+    else {
+        let temp = +arr[2];
+        arr[2] = temp + 10;
+    }
+    let rgbNew = arr.join(", ");
+    console.log(rgbNew)
+}
+
 function build(r, c){
-    let cont = document.getElementById('container');
-    while(cont.firstChild){
-        cont.removeChild(cont.firstChild);
+    //reset grid
+    let contain = document.getElementById('container');
+    while(contain.firstChild){
+        contain.firstChild.removeEventListener('mouseenter', hover(item));
+        contain.removeChild(contain.firstChild);
     }
     let height = r,
         width = c;
     html.style.setProperty('--rows', r);
     html.style.setProperty('--columns', c);
+    //populate grid
     for (i = 0; i < height * width; i++){
         let div = document.createElement('div');
-        div.classList.add('grid-item');
+        div.classList.add('grid-item', 'new');
         div.id = i +1;
         div.textContent = ' ';
-        cont.appendChild(div);
+        contain.appendChild(div);
     }
     
     let items = document.querySelectorAll('.grid-item');
     items.forEach((item) => {
-        item.addEventListener('mouseenter', () => {
-            item.classList.toggle('hovering');
+        item.addEventListener('mouseenter', (event) =>{
+            hover(item);
         });
     });
 }
+
+const html = document.querySelector('html');
 build(16, 16);
